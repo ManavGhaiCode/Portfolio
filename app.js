e1c7154aac68b5bdf4668b198eb084aa2fb658f1
ManavGhaiCode <manavghai@gmail.com>
2022-10-05 10:34:09 +0530

    add fancy text animation

--- app.js
+++ app.js
@@ -1,4 +1,33 @@
 const cursor = document.querySelector('#cursor');
+const rawSign = document.querySelector('#name');
+const sign = document.querySelector('#name').textContent.split('');
+
+rawSign.innerHTML = '';
+
+sign.forEach(el => {
+    rawSign.innerHTML += `<span class="name-text">${el}</span>`;
+});
+
+const tick = () => {
+    const char = rawSign.querySelectorAll('span')[charIndex];
+    char.classList.add('fade');
+
+    charIndex++;
+
+    if (charIndex === 10) {
+        complete();
+        return;
+    }
+}
+
+function complete() {
+    clearInterval(timer);
+    timer = null;
+}
+
+let charIndex = 0;
+let timer = setInterval(tick, 500);
+
 
 window.addEventListener("mousemove", e => {
     cursor.style.display = "block";
